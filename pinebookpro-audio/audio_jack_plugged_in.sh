e37f8324b1c1853825b48f45139fb450ee5ec9f6
yjun <jerrysteve1101@gmail.com>
2026-01-10 16:42:00 +0800

    pinebookpro-audio: add -c 0 to fix amixer failure
    When executing the audio_jack_plugged_in.sh script, errors occur:
    
    ```
    amixer: Unable to find simple control 'Speaker',0
    amixer: Unable to find simple control 'Playback Polarity',0
    ```
    
    It appears that the amixer command now requires specifying the audio card
    via the -c parameter. Adding -c 0 will allow the script to run successfully.
    
    Signed-off-by: yjun <jerrysteve1101@gmail.com>

--- pinebookpro-audio/audio_jack_plugged_in.sh
+++ pinebookpro-audio/audio_jack_plugged_in.sh
@@ -2,9 +2,9 @@
 
 case "$3" in
 	plug)
-		amixer sset Speaker off
-		amixer sset 'Playback Polarity' Normal ;;
+		amixer -c 0 sset Speaker off
+		amixer -c 0 sset 'Playback Polarity' Normal ;;
 	unplug)
-		amixer sset 'Playback Polarity' 'R Invert'
-		amixer sset Speaker on ;;
+		amixer -c 0 sset 'Playback Polarity' 'R Invert'
+		amixer -c 0 sset Speaker on ;;
 esac
